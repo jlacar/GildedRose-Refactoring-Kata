1bf672a919eca18105f9802fdf99e8d1d7aae6b7
Junilu Lacar <844560+jlacar@users.noreply.github.com>
2024-08-28 10:18:05 -0400

    r - Reformat Item parameters

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -1,6 +1,10 @@
 package com.gildedrose
 
-class Item(@JvmField val name: String, @JvmField var sellIn: Int, @JvmField var quality: Int) {
+class Item(
+    @JvmField val name: String,
+    @JvmField var sellIn: Int,
+    @JvmField var quality: Int
+) {
     override fun toString(): String {
         return "${this.name}, ${this.sellIn}, ${this.quality}"
     }
